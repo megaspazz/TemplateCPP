f6a4c8b8b441e37dcbbb2f07ecb407b16b818d86
Eric Pan <erzpan@gmail.com>
2016-09-29 20:30:10 -0500

    minor updates to template and run script

--- run.bat
+++ run.bat
@@ -5,14 +5,18 @@ SETLOCAL
 FOR /f "delims=" %%F IN ('dir /b /s "src\%1.cpp" 2^>NUL') DO SET filepath=%%F
 FOR %%F IN (%filepath%) DO SET filename=%%~nF
 
+SET INPUT=io\in.txt
+SET OUTPUT=io\out.txt
+
 SET COMPILE=g++ %filepath% -o bin\%filename%.exe
 
 IF DEFINED filepath (
     ECHO Compiling:  %filepath%
     %COMPILE% ^
         && ECHO Compilation successful. ^
-        && bin\%filename%.exe < io\in.txt > io\out.txt ^
-        && ECHO Execution successful.
+        && bin\%filename%.exe < %INPUT% > %OUTPUT% ^
+        && ECHO Execution successful. ^
+        && ECHO. && type %OUTPUT%
 ) ELSE (
     ECHO ERROR: File not found!
 )
--- src/#templates/template.cpp
+++ src/#templates/template.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
 	std::ios::sync_with_stdio(false);
+
+    
 
     return 0;
 }
