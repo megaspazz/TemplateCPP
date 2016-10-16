638e94f45932ed0d394266c003b2efdd69d914aa
Eric <erzpan@gmail.com>
2016-10-16 11:07:54 -0500

    updated run script

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SETLOCAL
+SETLOCAL EnableDelayedExpansion
 
 FOR /f "delims=" %%F IN ('dir /b /s "src\%1.cpp" 2^>NUL') DO SET filepath=%%F
 FOR %%F IN (%filepath%) DO SET filename=%%~nF
@@ -13,11 +13,21 @@ SET COMPILE=g++ -std=c++11 -Wall "%filepath%" -o "bin\%filename%.exe"
 
 IF DEFINED filepath (
     ECHO Compiling:  %filepath%
-    %COMPILE% ^
-        && ECHO Compilation successful. ^
-        && "bin\%filename%.exe" < %INPUT% > %OUTPUT% 2> %ERROR% ^
-        && ECHO Execution successful. ^
-        && ECHO. && type %OUTPUT%
+    %COMPILE% 2> %ERROR%
+    IF ERRORLEVEL 1 (
+        TYPE "%ERROR%""
+        ECHO.
+        ECHO Compilation failed.  See "%ERROR%" for details.
+    ) ELSE (
+        ECHO Compilation successful.
+        ECHO.
+        "bin\%filename%.exe" < %INPUT% > %OUTPUT% 2> %ERROR%  ^
+            && SET result=Execution successful. ^
+            || SET result=The program crashed.
+        TYPE "%OUTPUT%"
+        ECHO.
+        ECHO !result!
+    )
 ) ELSE (
     ECHO ERROR: File not found!
 )
